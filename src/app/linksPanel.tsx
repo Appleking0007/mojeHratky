235b7e7126a6604e24fa5de2506225c4a411b445
pechafi <pechafi@student.uzlabina.cz>
2024-02-29 15:53:50 +0100

    TESTTES

--- src/app/linksPanel.tsx
+++ src/app/linksPanel.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const LinksPanel: FC = () => {
     return (
-        <div className="ml-[170px] mr-[170px] mt-[92px] flex flex-row bg-[#E6E6E6] content-start justify-between">
+        <div className="ml-[170px] mr-[170px] mt-[92px] flex flex-row bg-green-200 md:bg-gray-500 lg:bg-red-600 xl:bg-blue-600 2xl:bg-white content-start justify-between">
             <div className="mt-10 ml-10 flex flex-row h-16 grid gap-9 content-start">
                 <div>Home</div>
                 <div>Contacts</div>
